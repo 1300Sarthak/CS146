05c289e4e6f51b8bf47550d7e710b7bda0d66295
Sarthak Sethi <sarthakluv@gmail.com>
2024-03-13 17:08:21 -0700

    lab5

--- Labs/Lab5/Lab5.java
+++ Labs/Lab5/Lab5.java
@@ -1,5 +1,36 @@
 package Labs.Lab5;
 
 public class Lab5 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public class Solution {
+        public boolean isValidBST(TreeNode root) {
+            return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        }
+
+        public boolean isValidBST(TreeNode node, long lower, long upper) {
+            if (node == null)
+                return true;
+
+            int val = node.val;
+            if (val <= lower || val >= upper)
+                return false;
+
+            if (!isValidBST(node.right, val, upper))
+                return false;
+            if (!isValidBST(node.left, lower, val))
+                return false;
+
+            return true;
+        }
+    }
 
 }
---
+++ Labs/Lab5/Lab5.py
@@ -0,0 +1,21 @@
+class Lab5(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def isValidBST(root):
+    def helper(node, lower=float('-inf'), upper=float('inf')):
+        if not node:
+            return True
+        val = node.val
+        if val <= lower or val >= upper:
+            return False
+        if not helper(node.right, val, upper):
+            return False
+        if not helper(node.left, lower, val):
+            return False
+        return True
+
+    return helper(root)
